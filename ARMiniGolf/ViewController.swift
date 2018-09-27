2e5975fbe214abab8f66cc4e553b1485a9675fbd
Jamie Feeny <feeny.jamie@gmail.com>
2018-09-27 13:36:36 -0700

    fixed the crazy physics problem, it was an extra physicsbody on the course itself

--- ARMiniGolf/ViewController.swift
+++ ARMiniGolf/ViewController.swift
@@ -189,9 +189,9 @@ class ViewController: UIViewController {
     courseNode.position = SCNVector3(x,y,z)
     
     // TODO: Attach physics body to course node
-    let physicsBody = SCNPhysicsBody(type: .static, shape: nil)
-//    physicsBody.restitution = 0.1
-    courseNode.physicsBody = physicsBody
+//    let physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+////    physicsBody.restitution = 0.1
+//    courseNode.physicsBody = physicsBody
     
 //    courseNode.physicsBody?.isAffectedByGravity = false  //TEST CODE
     
@@ -370,7 +370,7 @@ class ViewController: UIViewController {
       physicsBody.velocity = SCNVector3(0, 0, 0)
       physicsBody.angularVelocity = SCNVector4(0, 0, 0, 0)
     //globalBallNode.position = SCNVector3(courseNode.position.x, courseNode.position.y, courseNode.position.z + 3.8) //course1
-     globalBallNode.position = SCNVector3(courseNode.position.x - 0.5, courseNode.position.y + 0.25430253, courseNode.position.z + 2.7) //course2
+     globalBallNode.position = SCNVector3(courseNode.position.x - 0.5, courseNode.position.y + 0.2, courseNode.position.z + 2.7) //course2
     print("the balls position is\(globalBallNode.position)")
   }
   
