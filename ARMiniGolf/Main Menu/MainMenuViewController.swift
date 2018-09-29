fa63d4f7812419c7ac06c409e50098541f28df47
Jamie Feeny <feeny.jamie@gmail.com>
2018-09-29 12:09:43 -0700

    Added the constraint animation for the 3 levels

--- ARMiniGolf/Main Menu/MainMenuViewController.swift
+++ ARMiniGolf/Main Menu/MainMenuViewController.swift
@@ -11,7 +11,15 @@ import UIKit
 class MainMenuViewController: UIViewController {
   @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
   
-  @IBOutlet weak var startGameButton: UIButton!
+    @IBOutlet weak var course1CenterXConstraint: NSLayoutConstraint!
+    @IBOutlet weak var course2CenterXConstraint: NSLayoutConstraint!
+    @IBOutlet weak var course3CenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var course1ImageView: UIImageView!
+    @IBOutlet weak var course2ImageView: UIImageView!
+    @IBOutlet weak var course3ImageView: UIImageView!
+    
+    @IBOutlet weak var startGameButton: UIButton!
   
   override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +27,44 @@ class MainMenuViewController: UIViewController {
         // Do any additional setup after loading the view.
     startGameButton.clipsToBounds = true
     startGameButton.layer.cornerRadius = 10.0
+    //remove center x contraints on all 3
+    
+    let movementRightOffset = view.frame.width/2 + course1ImageView.frame.width/2
+      let movementLeftOffset = -1 * (view.frame.width/2 + course1ImageView.frame.width/2)
+    
+    course1CenterXConstraint.constant = movementRightOffset
+    course2CenterXConstraint.constant = movementLeftOffset
+    course3CenterXConstraint.constant = movementRightOffset
+    
+    UIView.animate(withDuration: 2, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, animations: {
+        self.course1CenterXConstraint.constant = 0
+        self.view.layoutIfNeeded()
+    })
+    UIView.animate(withDuration: 2, delay: 2, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, animations: {
+        self.course2CenterXConstraint.constant = 0
+        self.view.layoutIfNeeded()
+    })
+    UIView.animate(withDuration: 2, delay: 3, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, animations: {
+        self.course3CenterXConstraint.constant = 0
+        self.view.layoutIfNeeded()
+    })
+    
+    //set the c1 leading to the view trailing
+    
+    
+    //?? set the c1 trailing to the view trailing + c1.width ?? Do I need this if I have a width set?
+    
+  
+    
+    
+    //set the c2 trailing to the view leading
+    
+    //?? set the c1 trailing to the view leading + c1.width ?? Do I need this if I have a width set?
+    
+    //set the c3 leading to the view trailing
+    //?? set the c3 trailing to the view leading + c1.width ?? Do I need this if I have a width set?
+    
+    // then animate turning off the above 3 and turning on the 3 center ones
     }
     
 
