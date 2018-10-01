becd87d1f5d074964fb34b2e7c449078c03fcec2
Jamie Feeny <feeny.jamie@gmail.com>
2018-10-01 16:48:01 -0700

    tweaked the distance away from user and the size

--- ARMiniGolf/Model/GameManager.swift
+++ ARMiniGolf/Model/GameManager.swift
@@ -31,11 +31,11 @@ class GameManager: NSObject {
     //hard code the levels
     levels = [
 
-      Level.init(sceneFile: "art.scnassets/course.scn", musicFile: "background.mp3", initialCourseOffset: SCNVector3(-1, 0.05, -2.5),
+      Level.init(sceneFile: "art.scnassets/course.scn", musicFile: "background.mp3", initialCourseOffset: SCNVector3(-0.6, 0, -2.5),
                          initialBallOffset: SCNVector3(0, 0, 3.8), scale: 0.5),  // Level 1 - Placement at 50% is good
-      Level.init(sceneFile: "art.scnassets/course2.scn", musicFile: "background2.mp3", initialCourseOffset: SCNVector3(-0, 0, -2),
+      Level.init(sceneFile: "art.scnassets/course2.scn", musicFile: "background2.mp3", initialCourseOffset: SCNVector3(0, 0, -2),
                  initialBallOffset: SCNVector3(-0.5, 0.2, 2.7), scale: 0.5),  // Level 2
-      Level.init(sceneFile: "art.scnassets/course3.scn", musicFile: "background3.mp3", initialCourseOffset: SCNVector3(-0.5, 0, -2),
+      Level.init(sceneFile: "art.scnassets/course3.scn", musicFile: "background3.mp3", initialCourseOffset: SCNVector3(-0.25, 0, -2.5),
                   initialBallOffset: SCNVector3(0, 0.2, -3.924), scale: 0.6) // Level 3
     ]
   }
